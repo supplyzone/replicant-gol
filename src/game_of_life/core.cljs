c9295c21a9058a6372234fc481af27a34e6e2784
GUnit <galv4nized@gmail.com>
2025-04-21 10:31:28 +0200

    Refactor functions

--- src/game_of_life/core.cljs
+++ src/game_of_life/core.cljs
@@ -6,32 +6,41 @@
 (def square-length 20) ;; pixel
 (def gap 1) ;; pixel
 
-(defn start-new-game [store rows cols]
 
-  (let [board (game/create-game {:cols cols :rows rows})]
+(defn calculate-board-dimensions []
+  (let [board-el (js/document.querySelector ".board")
+        board-container-width (.-offsetWidth board-el)
+        board-container-height (.-offsetHeight board-el)
+        num-cols (Math/floor (quot board-container-width (+ square-length gap)))
+        num-rows (Math/floor (quot board-container-height (+ square-length gap)))]
+
+    {:cols num-cols :rows num-rows}))
+
+(defn reset-board [store]
+  (let [board-dimensions (calculate-board-dimensions)
+        cols (:cols board-dimensions)
+        rows (:rows board-dimensions)
+        board (game/create-game {:cols cols :rows rows})]
     (reset! store board)))
+
+
+(defn perform-action [store action args]
+  (case action
+    :flip-cell (apply swap! store game/mark-cell args)
+    :reset (reset-board store)
+    :simulate (swap! store game/toggle-tic-func store)
+    :pause (swap! store game/toggle-tic-func store)
+    :randomize (swap! store game/randomize store)))
 
 (defn main []
   (let [store (atom nil)
-        el (js/document.getElementById "app")
-        board-el (js/document.querySelector ".board")
-        board-container-width (.-offsetWidth board-el)
-        board-container-height (.-offsetHeight board-el)
-        cols (Math/floor (quot board-container-width (+ square-length gap)))
-        rows (Math/floor (quot board-container-height (+ square-length gap)))
-        ;
-        ]
+        el (js/document.getElementById "app")]
+
+    (.addEventListener js/window "resize" (fn [_] (reset-board store)))
 
     (r/set-dispatch!
      (fn [_ [action & args]]
-       (case action
-         :flip-cell (apply swap! store game/mark-cell args)
-         :reset (start-new-game store rows cols)
-         :simulate (swap! store game/toggle-tic-func store)
-         :pause (swap! store game/toggle-tic-func store)
-         :randomize (swap! store game/randomize store)
-         ;
-         )))
+       (perform-action store action args)))
 
     (add-watch store ::render
                (fn [_ _ _ game]
@@ -41,6 +50,6 @@
                   (r/render el))))
 
     ;; init first draw
-    (start-new-game store rows cols))
-
-  ())
+    (reset-board store))
+    ;
+  )
